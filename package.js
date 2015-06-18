403e46fe97279fb3c95e885c8f3b44311e36800e
Max Ferguson <maxkferg@gmail.com>
2015-06-17 17:36:26 -0700

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maxkferg:thumbnails',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'Create and cache thumbnails on the client',
   git: 'https://github.com/NitroLabs/meteor-thumbnails',
   documentation: 'README.md'
